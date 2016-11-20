411e7ab028198693671cdbd935e53dbc964934c2
colblitz <z.joseph.lee.z@gmail.com>
2016-11-20 00:10:21 -0800

    new templates, following tutorial

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
