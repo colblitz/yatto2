6ca7198492619cde5d040684de36510aad6445b3
colblitz <z.joseph.lee.z@gmail.com>
2016-11-19 23:23:00 -0800

    add scripts and test files

---
+++ scripts/ProcessCSVs.py
@@ -0,0 +1,18 @@
+import re
+with open('ArtifactInfo.csv', 'r') as f:
+	f.readline()
+	alls = []
+	for line in f:
+		s = re.sub(r'"([\w\s]+),([\w\s]+)"', r'\1:\2', line).strip().split(',')
+		aid = s[0][8:]
+		name = '"' + s[9] + '"'
+		costc = float(s[6])
+		coste = float(s[7])
+		maxLevel = int(s[1])
+		adpl = int(float(s[5]) * 100)
+		bType = 'BonusType.' + s[3]
+		bAmount = float(s[4]) * 100
+		alls.append('{:>2}: new Artifact({:>2}, {:24}, {:4.2f}, {:3.1f}, {:2d}, [[BonusType.ArtifactDamage, {:3d}], [{:34}, {:5.1f}]]),'.format(aid, aid, name, costc, coste, maxLevel, adpl, bType, bAmount))
+	alls.sort()
+	for s in alls:
+		print s
