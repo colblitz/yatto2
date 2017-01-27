18f08cd08bc8a685c5369e6fcbb15d24c9eb0f17
colblitz <z.joseph.lee.z@gmail.com>
2017-01-26 20:02:07 -0500

    typo

--- app/components/Update.js
+++ app/components/Update.js
@@ -19,7 +19,7 @@ class Update extends React.Component {
               <li>Logs out when you reload</li>
               <li>Site locks up when optimizing instead of some sort of spinner</li>
               <li><b>A few people have reported YATTWO suggesting a lot of Stone of the Valrunes levels - if you're experiencing this too, make sure you have everything filled out (artifact, hero levels at MS, skills, equipment, pets) - better yet, load your information from your save file. For Android, this save file would be somewhere like /sdcard/Android/data/com.gavehivecorp.taptitans2/files/ISavableGlobal.adat, whereas for iOS I assume it's something like Apps->Tap Titans 2->Documents->ISavableGlobal.adat (going off of what it was for TT1)</b></li>
-              <li>For now, make sure you input your artifact levels before your equipment information, otherwise the equipment bonuses will get modified by the equipment boosting artifacts<li>
+              <li>For now, make sure you input your artifact levels before your equipment information, otherwise the equipment bonuses will get modified by the equipment boosting artifacts</li>
             </ul>
           </div>
         }
