3b03383871e5af08174c05a23f39a086be9bb46c
colblitz <z.joseph.lee.z@gmail.com>
2017-02-20 09:17:49 -0800

    add valentines equipment

--- app/components/Update.js
+++ app/components/Update.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { toggleUpdate, saveIfToken } from '../actions/actions';
 
-const lastUpdate = "2017-02-20-1";
+const lastUpdate = "2017-02-20-2";
 
 class Update extends React.Component {
   render() {
@@ -12,7 +12,7 @@ class Update extends React.Component {
           <div className="update-stuff">
             <h3>Update (2/20/2017)</h3>
             <ul>
-              <li><b>There's currently a problem with reading save files, most likely due to Valentine's equipment. Expect a fix by the end of today.</b></li>
+              <li><b>Fix has been added for Valentine's equipment. Let me know if you're still having issues with loading save files.</b></li>
               <li>HTTPS is now enabled (big thanks to <a href="https://www.reddit.com/user/mokrinsky">/u/mokrinsky</a>!) - if you're even reading this, that means that things worked. If you're not reading this, well... ¯\_(ツ)_/¯</li>
               <li>There have been a couple of bugfixes in the past few days, as always, let me know if you find more</li>
               <li>There have also been a few QOL things - a notification if some of your stuff isn't filled, a bit of color, tweaks to the artifact buying suggestions</li>
